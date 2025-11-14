6d80ddfd3425f121c0bfa30f76f1a37226a90650
Liviadiaxx <liviadiasxavier@hotmail.com>
2025-11-14 17:30:18 -0300

    Implementacao de Classe Controller, Collections e Tratamento de Exceptions

--- controller/ProdutoController.ts
+++ controller/ProdutoController.ts
@@ -1,53 +1,51 @@
-// controller/ProdutoController.ts
+
 
 import { Cosmetico } from "../model/cosmetico";
 import { ProdutoRepository } from "../repository/produtoRepository";
 
 export class ProdutoController implements ProdutoRepository {
-    private listaProdutos: Array<Cosmetico> = []; // Collection
+    private listaProdutos: Array<Cosmetico> = []; 
     private idContador: number = 0; 
     
     public gerarId(): number {
         return ++this.idContador;
     }
 
-    // C - Create
+    
     public cadastrar(cosmetico: Cosmetico): void {
         this.listaProdutos.push(cosmetico);
-        console.log(`\n✅O produto '${cosmetico.nome}' (ID: ${cosmetico.id}) foi cadastrado com sucesso!`);
+        console.log(`\nO produto '${cosmetico.nome}' (ID: ${cosmetico.id}) foi cadastrado com sucesso!`);
     }
 
-    // R - Read (Listar Todos)
+    
     public listarTodos(): void {
         if (this.listaProdutos.length === 0) {
              console.log("\n Não há produtos cadastrados no estoque.");
              return;
         }
         for (let produto of this.listaProdutos) {
-            produto.visualizar(); // Polimorfismo!
+            produto.visualizar();
         }
     }
 
-    // R - Read (Buscar por ID)
     public buscarPorId(id: number): Cosmetico | null {
         return this.listaProdutos.find(p => p.id === id) || null;
     }
 
-    // U - Update (com Exceptions)
+    
     public atualizar(cosmetico: Cosmetico): void {
         let buscaProduto = this.buscarPorId(cosmetico.id);
 
         if (buscaProduto) {
             buscaProduto.preco = cosmetico.preco; 
             buscaProduto.marca = cosmetico.marca;
-            console.log(`\n✅ Produto ID ${cosmetico.id} atualizado com sucesso!`);
+            console.log(`\nProduto ID ${cosmetico.id} atualizado com sucesso!`);
         } else {
-            // Lançando a Exception
+            
             throw new Error("ProdutoNotFoundException: Produto não encontrado para atualização."); 
         }
     }
 
-    // D - Delete (com Exceptions)
     public deletar(id: number): void {
         let index = this.listaProdutos.findIndex(p => p.id === id);
 
@@ -55,7 +53,7 @@ export class ProdutoController implements ProdutoRepository {
             this.listaProdutos.splice(index, 1);
             console.log(`\n Produto ID ${id} excluído com sucesso!`);
         } else {
-            // Lançando a Exception
+            
             throw new Error("ProdutoNotFoundException: Produto não encontrado para exclusão.");
         }
     }
